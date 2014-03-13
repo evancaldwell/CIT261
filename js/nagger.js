48bb130ec8aacc26ae4f2a95079f8ea1140292a0
Evan Caldwell <evancaldwell@yahoo.com>
2014-03-13 00:56:51 -0600

    reworked some of the wordcloud to test more entries, started some setup functions per the conversation with Br. Barney, re-tooledthe overlay transitions also per the conversation with Br. Barney

--- js/nagger.js
+++ js/nagger.js
@@ -3,7 +3,6 @@
 //Initialize the ability to talk to the server
 var server = new talkToServer('/backend/server.php')
 
-
 //Use the Following Funtion for all document.getElementById calls
 //Example: $('exampleID') will return the actual element for that ID
 function $(el) {
@@ -20,29 +19,71 @@ function allDone(theDataWeGotBack) {
 }
 
 
+//A function for setting up the page oncreate
+function setup() {
+	var newItemBtn = $('add-item-btn');
+	var newItemView = $('new-item');
+	newItemBtn.newItemView = newItemView;
+
+	var wordcloudBtn = $('wordcloud-btn');
+	var wordcloudView = $('wordcloud');
+	wordcloudBtn.wordcloudView = wordcloudView;
+}
+
+//onload
+function setup1() {
+	var container = $('container')
+	container.style.height = window.innerHeight
+	var viewWidth = container.offsetWidth
+	console.log(window.innerHeight)
+	// var overlays = document.getElementsByClassName('overlay')
+	// for (i=0; i<overlays.length; ++i) {
+	// 	overlays[i].style.width = viewWidth - 45
+	// }
+}
 
 // ============ page animation functions ============
-function showOverlay(el) {
-	var div = $("new-item");
-	div.className = div.className + " show-overlay";
+function showWordcloudView(el) {
+	var viewWidth = $('container').offsetWidth
+	var viewHeight = $('container').offsetHeight
+	var div = $('wordcloud')
+	div.style.width = viewWidth - 45 + 'px'
+	div.style.height = viewHeight - 90 + 'px'
+	div.style.left = '45px'
 }
 
-function hideOverlay(el, callback) {
-	var div = $("new-item");
-	div.className = div.className + " hide-overlay";
-
-	setTimeout(function () {div.className = "overlay"}, 300);
+function hideWordcloudView(el) {
+	var viewWidth = $('container').offsetWidth
+	var viewHeight = $('container').offsetHeight
+	var div = $('wordcloud')
+	div.style.left = '100%'
 }
 
-function collapse(el) {
-	el.className = "overlay";
+function showNewItemView(el) {
+	var viewWidth = $('container').offsetWidth
+	var viewHeight = $('container').offsetHeight
+	var div = $('new-item')
+	div.style.width = viewWidth - 45 + 'px'
+	div.style.height = viewHeight - 90 + 'px'
+	div.style.left = '45px'
 }
+
+function hideNewItemView(el) {
+	var viewWidth = $('container').offsetWidth
+	var viewHeight = $('container').offsetHeight
+	var div = $('new-item')
+	div.style.left = '100%'
+}
+
+
 
 
 
 // =========== Playing with the word cloud ================
 function buildWordArray (wordList) {
-	wordList = ['family', 'family', 'family', 'work', 'family', 'work', 'school', 'work', 'school', 'CIT', 'shopping', 'eat', 'shopping'];
+	wordList = ['family', 'family', 'family', 'work', 'family', 'work', 'school', 'work', 
+				'family', 'school', 'CIT', 'shopping', 'eat', 'shopping', 'cake', 'kids', 
+				'PTA', 'kids', 'groceries', 'groceries', 'kids'];
 	var objArr = [], prev, j = 0;
 
 	wordList = wordList.sort();
@@ -64,7 +105,7 @@ function buildWordArray (wordList) {
 	return objArr
 }
 
-function buildWordCloud(el) {
+function buildWordCloud(el, callback) {
 	var objects = buildWordArray() // TODO: need to pass in the actual tags
 	var div = $("wordcloud-inner")
 	var html = ""
@@ -73,6 +114,8 @@ function buildWordCloud(el) {
 		html += objects[key].tag + '</div>'
 	}
 	div.innerHTML = html
+
+	callback();
 }
 
 
